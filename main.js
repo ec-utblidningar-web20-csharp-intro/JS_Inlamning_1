ce32b89a1a068cc159c81bc0feebe3004ca719ca
Björn Strömberg <bjorn.stromberg@codic.se>
2021-01-25 11:26:40 +0100

    Använder ES6 syntax. (let, arrow functions)

--- main.js
+++ main.js
@@ -1,43 +1,48 @@
-window.onload = function() {
-    document.getElementById("input-number").oninput = function (){
-        removeAllSections();
+document.addEventListener("DOMContentLoaded", () => {
+  let inputBox = document.querySelector("#input-number");
 
-        var count = parseInt(this.value);
-        for(var i = 0; i < count; i++){
+  inputBox.addEventListener("input", (event) => {
+    removeAllSections();
+    makeSections(parseInt(event.currentTarget.value));
+  });
+});
 
-            var parent = document.querySelector("main");
+let makeSections = (count) => {
+  for (var i = 0; i < count; i++) {
+    var parent = document.querySelector("main");
 
-            var child = document.createElement("section");
-            var title = document.createElement("h4");
-            var blurb = document.createElement("p");
+    var child = document.createElement("section");
+    var title = document.createElement("h4");
+    var blurb = document.createElement("p");
 
-            title.innerHTML = "Title " + i;
-            blurb.innerHTML = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore harum, quasi dicta ex totam quisquam quo tempore maxime, commodi praesentium eius quod suscipit! Tenetur magnam eligendi amet fugiat adipisci impedit.";
-            
-            makeEditable(title);
-            makeEditable(blurb);
+    title.innerText = "Title " + i;
+    blurb.innerText = `Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+      Inventore harum, quasi dicta ex totam quisquam quo tempore maxime, 
+      commodi praesentium eius quod suscipit! 
+      Tenetur magnam eligendi amet fugiat adipisci impedit.`;
 
-            child.appendChild(title);
-            child.appendChild(blurb);
-            parent.appendChild(child);
-        }
-    }
+    makeEditable(title);
+    makeEditable(blurb);
 
-}
+    child.append(title);
+    child.append(blurb);
+    parent.append(child);
+  }
+};
 
-function removeAllSections(){
-    var test_sections = document.querySelectorAll("section");
-    for(var i = 0; i < test_sections.length; i++){
-        test_sections[i].remove();
-    }
-}
+let removeAllSections = () => {
+  var test_sections = document.querySelectorAll("section");
+  for (var i = 0; i < test_sections.length; i++) {
+    test_sections[i].remove();
+  }
+};
 
-function makeEditable(elem){
-    elem.onclick = function(){
-        elem.contentEditable = true;
-        elem.focus();
-    }
-    elem.onblur = function(){
-        elem.contentEditable = false;
-    }
-}
+let makeEditable = (elem) => {
+  elem.addEventListener("click", (event) => {
+    elem.contentEditable = true;
+    elem.focus();
+  });
+  elem.addEventListener("blur", (event) => {
+    elem.contentEditable = false;
+  });
+};
